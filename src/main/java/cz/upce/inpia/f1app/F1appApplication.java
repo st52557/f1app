6426a28880e3b0eb07a2813ccd4151d500ccd53f
st52557 <43813109+st52557@users.noreply.github.com>
2022-03-14 14:55:48 +0100

    Initial commit

---
+++ src/main/java/cz/upce/inpia/f1app/F1appApplication.java
@@ -0,0 +1,13 @@
+package cz.upce.inpia.f1app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1appApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F1appApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/upce/inpia/f1app/F1appApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.upce.inpia.f1app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1appApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
